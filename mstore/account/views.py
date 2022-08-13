a1f64d2840c530943c17261c609ab1c5db485d17
Mrmahdi <ahangari138010@gmail.com>
2022-08-13 13:55:31 -0700

    template

--- mstore/account/views.py
+++ mstore/account/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 
 def sayHI(req):
-    return HttpResponse("salam")
+    return render(req, "account/index.html")
 
--- mstore/mstore/settings.py
+++ mstore/mstore/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'mstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["Templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR/"static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
